07ae9f607878b96e234aee4ef1a31991d23e7bfe
Nerijus <krekas@gmail.com>
2022-09-17 16:25:52 +0300

    Creating Model and API CRUD

---
+++ app/Http/Requests/CompanyRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CompanyRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required', 'string'],
+            'email' => ['required', 'email'],
+            'address' => ['nullable', 'string'],
+            'website' => ['nullable', 'url'],
+        ];
+    }
+}
