3eb9d95ece08137dcd44ec88bc3a2f432ddc1669
Nerijus <krekas@gmail.com>
2022-09-17 17:09:56 +0300

    List of Companies

--- resources/js/Pages/Companies/Index.jsx
+++ resources/js/Pages/Companies/Index.jsx
@@ -1,5 +1,63 @@
-export default function PostsIndex() {
-    return (
-       <div>Hello world.</div>
-    );
+import { Component } from "react";
+
+class CompaniesIndex extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            companies: []
+        }
+    }
+
+    fetchCompanies() {
+        axios.get('/api/companies')
+            .then(response => this.setState({ companies: response.data.data }))
+    }
+
+    componentDidMount() {
+        this.fetchCompanies()
+    }
+
+    renderCompanies() {
+        return this.state.companies.map(company => <tr>
+            <td className="px-6 py-4 text-sm leading-5 text-gray-900 whitespace-no-wrap">{company.name}</td>
+            <td className="px-6 py-4 text-sm leading-5 text-gray-900 whitespace-no-wrap">{company.email}</td>
+            <td className="px-6 py-4 text-sm leading-5 text-gray-900 whitespace-no-wrap">{company.address}</td>
+            <td className="px-6 py-4 text-sm leading-5 text-gray-900 whitespace-no-wrap">{company.website}</td>
+        </tr>);
+    }
+
+    render() {
+        return (
+            <div className="overflow-hidden overflow-x-auto p-6 bg-white border-gray-200">
+                <div className="min-w-full align-middle">
+                    <table className="min-w-full divide-y divide-gray-200 border">
+                        <thead>
+                            <tr>
+                                <th className="px-6 py-3 bg-gray-50">
+                                    <span className="text-xs font-medium tracking-wider leading-4 text-left text-gray-500 uppercase">Name</span>
+                                </th>
+                                <th className="px-6 py-3 bg-gray-50">
+                                    <span className="text-xs font-medium tracking-wider leading-4 text-left text-gray-500 uppercase">Email</span>
+                                </th>
+                                <th className="px-6 py-3 bg-gray-50">
+                                    <span className="text-xs font-medium tracking-wider leading-4 text-left text-gray-500 uppercase">Address</span>
+                                </th>
+                                <th className="px-6 py-3 bg-gray-50">
+                                    <span className="text-xs font-medium tracking-wider leading-4 text-left text-gray-500 uppercase">Website</span>
+                                </th>
+                                <th className="px-6 py-3 bg-gray-50">
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody className="table-body">
+                            {this.renderCompanies()}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        )
+    }
 }
+
+export default CompaniesIndex
